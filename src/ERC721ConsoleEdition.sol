7fd54d5ff38eb405caeaa9390257cd8cabbaebba
chirag-bgh <chirag2bsingh@gmail.com>
2022-10-07 01:11:50 +0530

    ft: add Auth

--- src/ERC721ConsoleEdition.sol
+++ src/ERC721ConsoleEdition.sol
@@ -6,8 +6,21 @@
 
 pragma solidity 0.8.17;
 
+import "ERC721A-Upgradeable/ERC721AUpgradeable.sol";
+import "ERC721A-Upgradeable/IERC721AUpgradeable.sol";
+import "ERC721A-Upgradeable/ERC721AStorage.sol";
+import "./utils/OwnableRoles.sol";
+
+
 contract ERC721ConsoleEdittion {
-    
+
+    /// @dev a role every minter module must have in order to mint new tokens.
+    uint256 public constant minterRole = _ROLE_1;
+
+    /// @dev role for admin actions
+    uint256 public constant adminRole = _ROLE_0;
+
+
 }
 
 
