ba2211518d3874dd20bcc163752dd304ea328372
chirag-bgh <chirag2bsingh@gmail.com>
2022-10-07 00:47:15 +0530

    wip

--- src/CreatorConsole.sol
+++ src/CreatorConsole.sol
@@ -1,13 +1,15 @@
+// SPDX-License-Identifier: MIT
+
 /// @title CreatorConsole 
 /// @author https://github.com/chirag-bgh
 /// @notice Factory contract 
 
 pragma solidity 0.8.17;
 
-contract CreatorConsoleV1 {
+contract CreatorConsole {
 
 
-    
+
 
 
 
---
+++ src/ERC721ConsoleEdition.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+/// @title ERC721 ConsoleEditionV1
+/// @author https://github.com/chirag-bgh
+/// @notice Edition contract 
+
+pragma solidity 0.8.17;
+
+contract ERC721ConsoleEdittion {
+    
+}
+
+
