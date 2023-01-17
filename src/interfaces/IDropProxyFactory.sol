308b0eaead60aa6117a76e457dd589d3297c3cc9
chirag-bgh <chirag2bsingh@gmail.com>
2023-01-18 01:47:26 +0530

    feat: add DropProxy Factory

---
+++ src/interfaces/IDropProxyFactory.sol
@@ -0,0 +1,72 @@
+//SDPX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+interface IDropProxyFactory {
+    event ImplementationSet(address indexed dropImpelmentation);
+
+    /**
+     * @dev Emitted when an edition is created.
+     * @param consoleDrop The address of the edition.
+     * @param deployer     The address of the deployer.
+     * @param initData     The calldata to initialize consoleDrop via `abi.encodeWithSelector`.
+     * @param contracts    The list of contracts called.
+     * @param data         The list of calldata created via `abi.encodeWithSelector`
+     * @param results      The results of calling the contracts. Use `abi.decode` to decode them.
+     */
+    event DropCreated(
+        address indexed consoleDrop,
+        address indexed deployer,
+        bytes initData,
+        address[] contracts,
+        bytes[] data,
+        bytes[] results
+    );
+
+    /**
+     * @dev Thrown if the lengths of the input arrays are not equal.
+     */
+    error ArrayLengthsMismatch();
+
+    // =============================================================
+    //               PUBLIC / EXTERNAL WRITE FUNCTIONS
+    // =============================================================
+
+    function createDrop(
+        bytes32 salt,
+        bytes calldata initData,
+        address[] calldata contracts,
+        bytes[] calldata data
+    ) external returns (address consoleDrop, bytes[] memory results);
+
+    /**
+     * @dev Changes the consoleDrop implementation contract address.
+     *
+     * Calling conditions:
+     * - The caller must be the owner of the contract.
+     *
+     * @param newImplementation The new implementation address to be set.
+     */
+    function setNewImplementation(address newImplementation) external;
+
+    // =============================================================
+    //               PUBLIC / EXTERNAL VIEW FUNCTIONS
+    // =============================================================
+
+    /**
+     * @dev The address of the implementation.
+     * @return The configured value.
+     */
+    function dropImplementation() external returns (address);
+
+    /**
+     * @dev Returns the deterministic address for the consoleDrop clone.
+     * @param by   The caller of the {createDrop} function.
+     * @param salt The salt, generated on the client side.
+     * @return addr The computed address.
+     * @return exists Whether the contract exists.
+     */
+    function consoleDropAddress(address by, bytes32 salt)
+        external
+        view
+        returns (address addr, bool exists);
+}
